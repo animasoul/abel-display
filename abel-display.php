bc024172e4c2f0f213a633c6f36980d4559465e4
abe <abel.rogers@gmail.com>
2023-05-05 20:47:44 +0100

    Update abel-display.php

--- abel-display.php
+++ abel-display.php
@@ -6,20 +6,12 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Abel Rogers
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       abel-display
  *
  * @package           create-block
- */
-
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_abel_display_block_init()
 {
