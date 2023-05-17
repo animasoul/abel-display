9f54061728a1243415ffe5dc90178529c2e4d070
abe <abel.rogers@gmail.com>
2023-05-17 11:13:25 +0100

    update description

--- abel-display.php
+++ abel-display.php
@@ -2,11 +2,13 @@
 
 /**
  * Plugin Name:       Abel Display
- * Description: A Gutenberg plugin that allows the user to select pages or posts by category or tags and display them in different ways.
+ * Plugin URI:        https://www.ajpartnersltd.com/building-a-wordpress-gutenberg-block-plugin-a-revised-guide/
+ * Description:       A Gutenberg plugin that allows the user to select pages or posts by category or tags and display them in different ways.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            Abel Rogers
+ * Author URI:        https://www.ajpartnersltd.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       abel-display
